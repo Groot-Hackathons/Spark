1a858d90659b2ef62ac5870b6a21fd3249cd074f
akash <akashunni123@gmail.com>
2022-06-19 02:03:05 +0530

    Added map points dummy

--- app/src/main/java/com/example/spark/ChargingStations.kt
+++ app/src/main/java/com/example/spark/ChargingStations.kt
@@ -1,16 +1,24 @@
 package com.example.spark
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.spark.databinding.ChargingStationBinding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
+
 
 class ChargingStations: AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ChargingStationBinding
+
+    private var places: ArrayList<LatLng> = ArrayList<LatLng>()
+    var builder: LatLngBounds.Builder = LatLngBounds.Builder()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +39,37 @@ class ChargingStations: AppCompatActivity(), OnMapReadyCallback {
 
         var linearLayoutManager = LinearLayoutManager(this)
         recyclerView.layoutManager = linearLayoutManager
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment_charging
+        ) as? SupportMapFragment
+
+        places.add(LatLng(12.883295, 77.490052))
+        places.add(LatLng(12.88, 55.77))
+
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap, places)
+        }
 
     }
 
     override fun onMapReady(p0: GoogleMap) {
         TODO("Not yet implemented")
     }
+
+    private fun addMarkers(googleMap: GoogleMap, data: ArrayList<LatLng>) {
+        data.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title("Hello")
+                    .position(place)
+            )
+            builder.include(place)
+//            googleMap.moveCamera(CameraUpdateFactory.newLatLng(place))
+//            googleMap.animateCamera(CameraUpdateFactory.zoomTo(18.0f))
+        }
+        val bounds = builder.build()
+        val cu = CameraUpdateFactory.newLatLngBounds(bounds, 100)
+        googleMap.animateCamera(cu)
+    }
+
 }
