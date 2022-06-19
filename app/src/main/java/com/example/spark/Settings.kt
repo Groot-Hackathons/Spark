f3ab7d345cbae88b98376a308c9731536735f653
Shubham Bhardwaj <bhardwajshubham843@gmail.com>
2022-06-19 09:56:05 +0530

    Email Display

--- app/src/main/java/com/example/spark/Settings.kt
+++ app/src/main/java/com/example/spark/Settings.kt
@@ -3,6 +3,7 @@ package com.example.spark
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.spark.databinding.SettingsBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -15,6 +16,12 @@ class Settings: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = SettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        var firebaseAuth = FirebaseAuth.getInstance()
+        var currentUser = firebaseAuth.getCurrentUser()
+        var email = currentUser?.email
+        binding.userName.setText(email.toString())
+
+
         binding.logout.setOnClickListener {
             var firebaseAuth = FirebaseAuth.getInstance()
             firebaseAuth.signOut()
